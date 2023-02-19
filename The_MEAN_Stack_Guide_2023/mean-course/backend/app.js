3e6194c9254708b36047a59a7af46d40d764e1b7
Yuuka <ytamura@YMBA.local>
2023-02-19 12:36:14 -0500

    Section 4: MongoDB

--- The_MEAN_Stack_Guide_2023/mean-course/backend/app.js
+++ The_MEAN_Stack_Guide_2023/mean-course/backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://user1:LuexX3YMfWXIlQRd@cluster0.ynquu.mongodb.net/node-angular?retryWrites=true&w=majority")
+.then(() => {
+  console.log('Connected to db!');
+})
+.catch(() => {
+  console.log('Connection failed!');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
@@ -20,31 +31,32 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully!'
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then((createdPost) => {
+    res.status(201).json({
+      message: 'Post added successfully!',
+      postId: createdPost._id
+    });
   });
 });
 
 app.get( '/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'faedsea',
-      title: 'First server-side post!',
-      content: 'Hello!'
-    },
-    {
-      id: 'faedfjg',
-      title: 'Second server-side post!',
-      content: 'Good morning!'
-    }
-  ];
-  res.status(200).json({
-    message: 'Post fetched successfully!!!',
-    posts: posts
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: 'Post fetched successfully!!!',
+      posts: documents
+    });
   });
+});
 
+app.delete('/api/posts/:id', (req, res, next) => {
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Post deleted!"});
+  });
 });
 
 module.exports = app;
